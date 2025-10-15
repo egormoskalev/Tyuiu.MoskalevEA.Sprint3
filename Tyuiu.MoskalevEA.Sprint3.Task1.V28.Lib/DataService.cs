f70d13243dced70c4e64da70be5ee190ea721949
Егор Москалев <moskalevegor310@gmail.com>
2025-10-15 21:31:16 +0500

    Task1

--- Tyuiu.MoskalevEA.Sprint3.Task1.V28.Lib/DataService.cs
+++ Tyuiu.MoskalevEA.Sprint3.Task1.V28.Lib/DataService.cs
@@ -7,10 +7,10 @@ namespace Tyuiu.MoskalevEA.Sprint3.Task1.V28.Lib
         public double GetSumSeries(double value, int startValue, int stopValue)
         {
             double sum = 0;
-            int k;
-            for (k = startValue; k < stopValue; k++)
+            while (startValue <= stopValue)
             {
-                sum = sum + ((Math.Pow(value , k) + 0.25) * Math.Cos(k));
+                sum = sum + ((Math.Pow(value , startValue) + 0.25) * Math.Cos(startValue));
+                startValue++;
             }
             return Math.Round(sum, 3);
         }
