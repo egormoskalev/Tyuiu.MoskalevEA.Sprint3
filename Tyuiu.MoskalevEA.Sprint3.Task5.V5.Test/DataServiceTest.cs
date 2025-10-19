e08af7a4e1aae10e526189414f89563ebdb49144
Егор Москалев <moskalevegor310@gmail.com>
2025-10-19 14:36:26 +0500

    Task5

---
+++ Tyuiu.MoskalevEA.Sprint3.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MoskalevEA.Sprint3.Task5.V5.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint3.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint3.Task5.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MoskalevEA.Sprint3.Task5.V5.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint3.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 3;
+            int startValue2 = 10;
+            int stopValue1 = 1;
+            int stopValue2 = 1;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага 1= " + startValue1);
+            Console.WriteLine("Конец шага 1= " + stopValue1);
+            Console.WriteLine("Старт шага 2= " + startValue2);
+            Console.WriteLine("Конец шага 2= " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetSumSumSeries( x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
